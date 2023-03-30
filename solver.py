454b335dd154f1ed19a4fa7b8082b18640e5e8ae
can pooper <domesticatedhuman9999@gmail.com>
2023-03-30 17:13:50 -0400

    lmaogit add *git add *git add *git add *git add *git add *git add *git add *git add *git add *git add *git add *git add *git add *git add *git add *git add *git add *git add *git add *git add *git add *git add *

--- solver.py
+++ solver.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def solve_system(coefficients, eq):
-    inv_coefficients = np.linalg.inv(coefficients);
-    solution = np.dot(inv_coefficients, eq)
+def solve_system(A, B):
+    inv_A = np.linalg.inv(A);
+    solution = np.dot(inv_A, B)
     return solution
